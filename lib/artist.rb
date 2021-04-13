575a9fae1ee57dc04f78b6150e53f95c4ac6d801
Elliott Vargo <vargoelliott@gmail.com>
2021-04-13 04:06:32 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,6 +4,7 @@ require_relative '../config/environment'
 class Artist
   extend Findable
   extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   include Paramable
   attr_accessor :name
   attr_reader :songs
